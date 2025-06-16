9805c2ed2f6200c5cc8e767ac93f4e2a8b25922e
Ivan Amoshyi <amoshyiivan@gmail.com>
2025-06-16 13:33:04 +0300

    Added service layer for BE

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,19 +1,11 @@
-import { Superhero } from "../model/HeroModel.js";
-import HttpError from "../helpers/HttpError.js";
 import ctrlWrapper from "../helpers/ctrlWrapper.js";
-import path from "path";
-import fs from "fs/promises";
-
-const dirname = import.meta.dirname;
+import SuperheroService from "../services/SuperheroService.js";
 
 const getAllHeroes = async (req, res) => {
   const { page = 1, limit = 5 } = req.query;
 	const skip = (page - 1) * limit;
 
-  const [superheroes, total] = await Promise.all([
-		Superhero.find({}, "_id nickname images", { skip, limit: Number(limit) }).sort({ createdAt: -1 }),
-		Superhero.countDocuments(),
-	]);
+	const [superheroes, total] = await SuperheroService.getAllHeroes(skip, limit);
 
 	res.status(200).json({
 		superheroes,
@@ -23,55 +15,19 @@ const getAllHeroes = async (req, res) => {
 
 const getHeroById = async (req, res) => {
   const { id } = req.params;
-  const hero = await Superhero.findById(id);
-  if (!hero) throw HttpError(400, "No such hero");
+	const hero = await SuperheroService.getHeroById(id);
   res.status(200).json(hero);
 }
 
 const addHero = async (req, res) => {
 	const files = req.files;
-	const { nickname: newHeroNickname } = req.body;
-
-	const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-	const duplicateHero = await Superhero.findOne({
-		nickname: { $regex: `^${escapeRegex(newHeroNickname)}$`, $options: "i" },
-	});
-	if (duplicateHero) throw HttpError(400, "The hero with this nickname already exists");
-
-	const newHero = await Superhero.create({ ...req.body, images: [] });
-
-	const heroDirName = newHero._id.toString();
-	const imagesSaveDir = path.join(dirname, "../", "public", "images", heroDirName);
-
-	await fs.mkdir(imagesSaveDir, { recursive: true });
-  const imagesArray = [];
-
-  await Promise.all(
-    files.map(async (file) => {
-			const { originalname, path: oldPath } = file;
-
-      const imageUrl = path.join("images", heroDirName , originalname);
-      imagesArray.push(imageUrl);
-
-      await fs.rename(oldPath, path.join(imagesSaveDir, originalname));
-    })
-	)
-	
-	newHero.images = imagesArray;
-	await newHero.save();
-
+	const newHero = SuperheroService.addHero(files, req.body);
   res.status(201).json(newHero);
 }
 
 const deleteHero = async (req, res) => {
   const { id } = req.params;
-
-  const result = await Superhero.findByIdAndDelete(id);
-
-	if (!result) throw HttpError(400, "No such superhero");
-
-	const imagesFolderPath = path.join(dirname, "..", "public", "images", id);
-	await fs.rm(imagesFolderPath, { recursive: true, force: true });
+	await SuperheroService.deleteHero(id);
 
 	res.status(200).json({
 		message: "success",
@@ -80,67 +36,7 @@ const deleteHero = async (req, res) => {
 
 const editHero = async (req, res) => {
 	const { id } = req.params;
-	const heroToEdit = await Superhero.findById(id);
-
-	if (!heroToEdit) {
-		throw HttpError(400, "No such superhero");
-	}
-
-	let existingImages = [];
-	if (req.body.images) {
-		try {
-			existingImages = JSON.parse(req.body.images);
-		} catch {
-			existingImages = [];
-		}
-	}
-
-	const { images: allHeroImages } = heroToEdit;
-	const imagesToKeep = existingImages;
-
-	const imagesToDelete = allHeroImages.filter(
-		(img) => !imagesToKeep.includes(img)
-	);
-
-	await Promise.all(
-		imagesToDelete.map(async (imgPath) => {
-			const fullPath = path.join(dirname, "../", "public", imgPath);
-			try {
-				await fs.unlink(fullPath);
-			} catch (error) {
-				console.log( error.message);
-			}
-		})
-	);
-
-	const newFiles = req.files;
-	const newImages = [];
-
-	if (newFiles) {
-		const heroDirName = heroToEdit._id.toString();
-		const imagesSaveDir = path.join(dirname, "../", "public", "images", heroDirName);
-
-		await Promise.all(
-			newFiles.map(async (file) => {
-				const { originalname, path: oldPath } = file;
-
-				const imageUrl = path.join("images", heroDirName, originalname);
-				newImages.push(imageUrl);
-
-				await fs.rename(oldPath, path.join(imagesSaveDir, originalname));
-			})
-		);
-	}
-
-	const updatedHero = await Superhero.findByIdAndUpdate(
-		id,
-		{
-			...req.body,
-			images: [...existingImages, ...newImages],
-		},
-		{ returnDocument: "after" }
-	);
-
+	const updatedHero = await SuperheroService.editHero(id, req.body, req.files);
 	res.status(200).json(updatedHero);
 };
 
---
+++ server/services/SuperheroService.js
@@ -0,0 +1,142 @@
+import { Superhero } from "../model/HeroModel.js";
+import HttpError from "../helpers/HttpError.js";
+import fs from "fs/promises";
+import path from "path";
+
+const dirname = import.meta.dirname;
+
+const SuperheroService = {
+	getAllHeroes: async (skip, limit) => {
+		return await Promise.all([
+			Superhero.find({}, "_id nickname images", {
+				skip,
+				limit: Number(limit),
+			}).sort({ createdAt: -1 }),
+			Superhero.countDocuments(),
+		]);
+	},
+	getHeroById: async (id) => {
+		const hero = await Superhero.findById(id);
+		if (!hero) throw HttpError(400, "No such hero");
+		return hero;
+	},
+	addHero: async (files, body) => {
+		const { nickname: newHeroNickname } = body;
+
+		const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+		const duplicateHero = await Superhero.findOne({
+			nickname: { $regex: `^${escapeRegex(newHeroNickname)}$`, $options: "i" },
+		});
+		if (duplicateHero)
+			throw HttpError(400, "The hero with this nickname already exists");
+
+		const newHero = await Superhero.create({ ...body, images: [] });
+
+		const heroDirName = newHero._id.toString();
+		const imagesSaveDir = path.join(
+			dirname,
+			"../",
+			"public",
+			"images",
+			heroDirName
+		);
+
+		await fs.mkdir(imagesSaveDir, { recursive: true });
+		const imagesArray = [];
+
+		await Promise.all(
+			files.map(async (file) => {
+				const { originalname, path: oldPath } = file;
+
+				const imageUrl = path.join("images", heroDirName, originalname);
+				imagesArray.push(imageUrl);
+
+				await fs.rename(oldPath, path.join(imagesSaveDir, originalname));
+			})
+		);
+
+		newHero.images = imagesArray;
+		await newHero.save();
+		return newHero;
+	},
+	deleteHero: async (id) => {
+		const result = await Superhero.findByIdAndDelete(id);
+
+		if (!result) throw HttpError(400, "No such superhero");
+
+		const imagesFolderPath = path.join(dirname, "..", "public", "images", id);
+		await fs.rm(imagesFolderPath, { recursive: true, force: true });
+	},
+	editHero: async (id, body, newFiles) => {
+		const heroToEdit = await Superhero.findById(id);
+
+		if (!heroToEdit) {
+			throw HttpError(400, "No such superhero");
+		}
+
+		let existingImages = [];
+		if (body.images) {
+			try {
+				existingImages = JSON.parse(body.images);
+			} catch {
+				existingImages = [];
+			}
+		}
+
+		const { images: allHeroImages } = heroToEdit;
+		const imagesToKeep = existingImages;
+
+		const imagesToDelete = allHeroImages.filter(
+			(img) => !imagesToKeep.includes(img)
+		);
+
+		await Promise.all(
+			imagesToDelete.map(async (imgPath) => {
+				const fullPath = path.join(dirname, "../", "public", imgPath);
+				try {
+					await fs.unlink(fullPath);
+				} catch (error) {
+					console.log(error.message);
+				}
+			})
+		);
+
+		const newImages = [];
+
+		if (newFiles) {
+			const heroDirName = heroToEdit._id.toString();
+			const imagesSaveDir = path.join(
+				dirname,
+				"../",
+				"public",
+				"images",
+				heroDirName
+			);
+
+			await Promise.all(
+				newFiles.map(async (file) => {
+					const { originalname, path: oldPath } = file;
+
+					const imageUrl = path.join("images", heroDirName, originalname);
+					newImages.push(imageUrl);
+
+					await fs.rename(oldPath, path.join(imagesSaveDir, originalname));
+				})
+			);
+		}
+
+		const updatedHero = await Superhero.findByIdAndUpdate(
+			id,
+			{
+				...body,
+				images: [...existingImages, ...newImages],
+			},
+			{ returnDocument: "after" }
+    );
+
+    return updatedHero;
+	},
+};
+
+export default SuperheroService;
